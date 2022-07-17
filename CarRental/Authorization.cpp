9ea63f389a97e0945d533199be457e618c44dbe8
hontur <noanonofi@yandex.ru>
2022-07-17 11:38:34 +0500

    Навел порядок в системе авторизации

--- CarRental/Authorization.cpp
+++ CarRental/Authorization.cpp
@@ -7,42 +7,68 @@ void Authorization::RegistrartionSystem()
 	std::cout << "======================================" << std::endl;
 	std::cout << "\tCar Rental System" << std::endl;
 	std::cout << "======================================" << std::endl << std::endl;
-	std::cout << "Register an account:" << std::endl;
+
+	std::cout << "Register an account:" << std::endl << std::endl;
 
 	//==============================================================================
 
-	std::cout << "Enter your login: ";
-	std::cin >> login;
-	std::cout << "\nCheck...." << login << std::endl << std::endl;
-	Sleep(500);
+	/*
+	* Checking for login input until it is entered correctly
+	*/
 
-	if (list.searchLogin(login)) {
-		std::cout << "Login is correct" << std::endl << std::endl;
-		profileINFO.setLoginClient(login);
-	} else std::cout << "Login is uncorrect, maybe login lenght < 5 symbol" << std::endl;
-
-	//==============================================================================
-
-	std::cout << "Enter your email: ";
-	std::cin >> email;
-	std::cout << "\nCheck...." << email << std::endl << std::endl;
-	Sleep(500);
-
-	if (list.searchEmail(email)) {
-		std::cout << "Email is correct" << std::endl << std::endl;
-		profileINFO.setLoginClient(email);
-	} else std::cout << "Email is uncorrect, maybe email lenght < 5 symbol" << std::endl;
+	do {
+		std::cout << "Enter your username: ";
+		std::cin >> login;
+		std::cout << "\nCheck...." << login << std::endl << std::endl;
+		Sleep(500);
+		if (list.searchLogin(login)) {
+			std::cout << "Login is correct" << std::endl << std::endl;
+			profileINFO.setLoginClient(login);
+		}
+		else { std::cout << "Login is uncorrect, maybe login lenght < 5 symbol" << std::endl << std::endl; }
+	} while (!list.searchLogin(login));
 
 	//==============================================================================
 
-	std::cout << "Enter your password: ";
-	std::cin >> password;
-	std::cout << "\nCheck...." << password << std::endl << std::endl;
-	Sleep(500);
+	/*
+	* Checking for e-mail input until it is entered correctly
+	*/
 
-	if (checkCorrectInput.checkPasswordClient(password)) {
-		std::cout << "Password is correct" << std::endl;
-	} else std::cout << "Password is uncorrect, maybe login lenght < 8 symbol" << std::endl;
+	do {
+		std::cout << "Enter your email: ";
+		std::cin >> email;
+		std::cout << "\nCheck...." << email << std::endl << std::endl;
+		Sleep(500);
+
+		if (list.searchEmail(email)) {
+			std::cout << "Email is correct" << std::endl << std::endl;
+			profileINFO.setLoginClient(email);
+		}
+		else { std::cout << "Email is uncorrect, maybe email lenght < 5 symbol" << std::endl; exit(1); }
+	} while (!list.searchEmail(email));
+
+	//==============================================================================
+
+	/*
+	* Checking for entering a password until it meets the conditions
+	*/
+
+	do {
+		std::cout << "Enter your password: ";
+		std::cin >> password;
+		std::cout << "\nCheck...." << password << std::endl << std::endl;
+		Sleep(500);
+
+		if (checkCorrectInput.checkPasswordClient(password)) {
+			std::cout << "Password is correct" << std::endl;
+		}
+		else { std::cout << "Password is uncorrect, maybe login lenght < 8 symbol" << std::endl;}
+	} while (!checkCorrectInput.checkPasswordClient(password));
+
+
+	std::cout << "======================================" << std::endl;
+	std::cout << "\tRegistration is success" << std::endl;
+	std::cout << "======================================" << std::endl << std::endl;
 }
 
 void Authorization::AuthorizationSystem()
@@ -50,16 +76,17 @@ void Authorization::AuthorizationSystem()
 	std::cout << "======================================" << std::endl;
 	std::cout << "\tCar Rental System" << std::endl;
 	std::cout << "======================================" << std::endl << std::endl;
-	std::cout << "Sing in account:" << std::endl;
+
+	std::cout << "Sing in account:" << std::endl << std::endl;
 
 	//==============================================================================
 
 	std::string SingInLogin, SingInPassword;
 
-	std::cout << "Enter your login: ";
+	std::cout << "Enter your login specified during registration: ";
 	std::cin >> SingInLogin;
 
-	std::cout << "Enter your password: ";
+	std::cout << "Enter your password specified during registration: ";
 	std::cin >> SingInPassword;
 
 	//==============================================================================
